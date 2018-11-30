339528532b978d29aa017d36b0e084b68916febf
jdm79 <james.malvern@gmail.com>
2018-11-30 14:37:38 +0000

    sets up test folders, adds enzyme, setuptest.js

---
+++ __tests__/peep-display-test.js
@@ -0,0 +1,10 @@
+/*
+
+As a maker
+So that I can see what others are saying  
+I want to see all peeps in reverse chronological order
+
+*/
+
+// fetch the peeps, use reverse, display them
+
---
+++ __tests__/peep-fetcher-test.js
@@ -0,0 +1,5 @@
+describe('Fetch peeps' () => {
+  it('fetches the peeps from the api', () => {
+    expect(peeps).toBe(Array)
+  })
+})
