a62110d66de9f92952a670b85d3f6a92b9e5ff9d
jdm79 <james.malvern@gmail.com>
2018-12-05 16:07:51 +0000

    test: tests that the url of page clicked on matches expectation

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -2,6 +2,9 @@ describe('My first test', () => {
   it('visits the kitchen sink', () => {
     cy.visit('https://example.cypress.io')
 
+    // this is an example of chaining #declarative
     cy.contains('type').click()
+
+    cy.url().should('include', '/commands/actions')
   })
 })
