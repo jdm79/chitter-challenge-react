8b67e049c302fb83ce24285c0bc17ae48e5207f3
jdm79 <james.malvern@gmail.com>
2018-12-05 16:10:58 +0000

    test: fills input with email, checks it matches expectation

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -6,5 +6,10 @@ describe('My first test', () => {
     cy.contains('type').click()
 
     cy.url().should('include', '/commands/actions')
+
+    // Get an input, type into it and verify that the value has been updated
+    cy.get('.action-email')
+      .type('james.malvern@gmail.com')
+      .should('have.value', 'james.malvern@gmail.com')
   })
 })
