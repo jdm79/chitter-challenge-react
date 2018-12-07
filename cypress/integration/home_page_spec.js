91c0967497c3c251732de5d7404c12d9d2684411
jdm79 <james.malvern@gmail.com>
2018-12-07 12:31:21 +0000

    test: adds first failing test: create a log in button on home page

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -2,6 +2,8 @@ describe('Home page', () => {
   it('successfully loads the home page', () => {
     cy.visit('/')
     cy.contains('Chitter Frontend')
+    cy.url().should('eq', 'http://localhost:3000/')
+    cy.contains('Log in').click()
 
   })
 })
