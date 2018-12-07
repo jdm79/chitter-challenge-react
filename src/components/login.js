1a638974662070224596412d0af9c2d329912ce4
jdm79 <james.malvern@gmail.com>
2018-12-07 20:13:58 +0000

    chore: adds an alert to remind user to fill in the fields before submitting

--- src/components/login.js
+++ src/components/login.js
@@ -10,17 +10,17 @@ class LoginForm extends Component {
       password: ''
     };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
   // this uses ES6 to access the name of input and the value from it
   // saves having to write lots of handleChanges
   handleChange(event) {
-    this.setState({
-      [event.target.name]: event.target.value
-    });
-    this.handleLoginClick()
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+      this.handleLoginClick()
   }
 
   handleLoginClick() {
@@ -28,22 +28,24 @@ class LoginForm extends Component {
   }
 
   handleSubmit(event){
-    alert(`Success! Email: ${this.state.email}, password: ${this.state.password} and logged-in status: ${this.state.isLoggedIn} `);
-    event.preventDefault();
-    this.setState({ email: '', password: '' })
+    if(this.state.email.length > 0 && this.state.password.length > 0) {
+      alert(`Success! Email: ${this.state.email}, password: ${this.state.password} and logged-in status: ${this.state.isLoggedIn} `)
+      event.preventDefault()
+      this.setState({ email: '', password: '' })
+    } else {
+      alert("Fields cannot be blank")
+      event.preventDefault()
+
+    }
   }
 
   render() {
     return (
       <form onSubmit={this.handleSubmit} className="login-form">
-        <label className="label">
-          Email: 
-          <input type="email" name="email" value={this.state.email} className="email" onChange={this.handleChange} />
-        </label>
-        <label className="label">
-          Password: 
-          <input type="password" name="password" value={this.state.password} className="password" onChange={this.handleChange} />
-        </label>
+        <input type="email" name="email" value={this.state.email} placeholder="Email" 
+        className="email" onChange={this.handleChange} />
+        <input type="password" name="password" value={this.state.password} placeholder="Password"
+        className="password" onChange={this.handleChange} />
         <input type="submit" value="Log in" />
       </form>
     );
