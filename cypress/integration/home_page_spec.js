4d780399d7614f43c719008cdbf1f878082d77bf
jdm79 <james.malvern@gmail.com>
2018-12-07 17:27:58 +0000

    test: refactors test to find email input and fill it in, then check the value

--- cypress/integration/home_page_spec.js
+++ cypress/integration/home_page_spec.js
@@ -3,7 +3,16 @@ describe('Home page', () => {
     cy.visit('/')
     cy.contains('Chitter Frontend')
     cy.url().should('eq', 'http://localhost:3000/')
-    cy.contains('Log in').click()
+  })
 
+  it('fills in log in form and clicks button', () => {
+    // add tests to fill in email
+    cy.get('.email')
+      .type('james.malvern@email.com')
+      .should('have.value', 'james.malvern@email.com')
+    // add test to fill in password
+    // cy.contains('Password').type('password1234')
+    // // add test to click log in
+    // cy.contains('Log in').click()
   })
 })
